499fac98170bdc39ec6349549bb0753baf634ee7
Champetier <jb.champetier@gmail.com>
2022-05-03 19:41:57 +0200

    html controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import com.nnk.springboot.repositories.UserRepository;
 import com.nnk.springboot.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -23,27 +24,47 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         this.userRepository = userRepository;
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
-        auth.inMemoryAuthentication().withUser("a").password("a").roles("ADMIN");
+    /**
+     * Authentication provider dao authentication provider.
+     *
+     * @return the dao authentication provider
+     */
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
-   @Override
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+
+    @Override
     public void configure(HttpSecurity http) throws Exception{
-        /*http.authorizeRequests()
-                .antMatchers("/register").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/register","/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
-                .and()
-                .rememberMe()
-                .key("rem-me-key")
-                .rememberMeParameter("rememberme") // it is name of checkbox at login page
-                .rememberMeCookieName("remembermelogin")
-                .and()
-                .logout();
-       http.csrf().disable();*/
+                .cors().and().csrf().disable()
+               .formLogin()
+               .loginPage("/login")
+               .permitAll()
+               .failureUrl("/")
+               .defaultSuccessUrl("/home", true)
+               .and()
+               .logout()
+               .logoutUrl("/logout")
+               .logoutSuccessUrl("/")
+               .clearAuthentication(true)
+               .invalidateHttpSession(true)
+               .deleteCookies("JSESSIONID")
+               .permitAll()
+               .and()
+               .rememberMe().key("UniqueKey").tokenValiditySeconds(3600);
     }
 
     @Bean
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -1,15 +1,22 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.configuration.SpringSecurityConfig;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController
 {
 	@RequestMapping("/")
-	public String home(Model model)
+	public String home(Principal user, Model model)
 	{
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		model.addAttribute("security", authentication.getName());
 		return "home";
 	}
 
