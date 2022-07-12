c50d2141e8f09f8671a3ec27f491372106ffbd30
Champetier <jb.champetier@gmail.com>
2022-07-12 14:01:58 +0200

    test

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -4,6 +4,7 @@ import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +17,12 @@ public class LoginController {
 
     @GetMapping("login")
     public ModelAndView login() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
+    }
+    @PostMapping("app-logout")
+    public ModelAndView logout() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
         return mav;
