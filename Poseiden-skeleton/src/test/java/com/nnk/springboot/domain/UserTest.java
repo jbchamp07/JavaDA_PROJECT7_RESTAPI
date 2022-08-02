42b52211878af169e5d28a4c2ebaba88cda15ce7
Champetier <jb.champetier@gmail.com>
2022-08-02 19:13:21 +0200

    root

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/domain/UserTest.java
+++
@@ -1,7 +0,0 @@
-package com.nnk.springboot.domain;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class UserTest {
-}
--- Poseiden-skeleton/src/test/java/com/nnk/springboot/repository/RatingRepositoryTest.java
+++
@@ -1,4 +0,0 @@
-package com.nnk.springboot.repository;
-
-public class RatingRepositoryTest {
-}
